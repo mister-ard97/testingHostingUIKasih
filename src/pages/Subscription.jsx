281c06fe03d3f7a59d95ec2c3f75334cfbf130fc
Firliandy <lian.eddy@gmail.com>
2019-10-17 17:37:44 +0700

    Subscription UI

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -2,47 +2,88 @@ import React, { Component } from 'react';
 import Numeral from 'numeral'
 import { connect } from 'react-redux'
 import { applySub, getSub } from '../redux/actions' 
+import { Redirect } from 'react-router-dom'
+import { Switch } from '@material-ui/core'
 
 class Subscription extends Component {
     state = { 
-        
+        redirectHome: false,
+        lain : false
     }
 
     componentDidMount(){
         if(!this.props.email){
-            return null
+            return this.setState({ redirectHome: true })
         }
         this.props.getSub(this.props.email)
     }
 
     getSubPrice = () => {
-        var subPrice = this.subPrice.value
+        if(!this.props.email){
+            return null
+        }
+        if(this.state.lain){
+            var subPriceBebas = this.refs.nominalBebas.value
+            if(subPriceBebas < 10000){
+                return window.alert('Harus diatas Rp. 10.000')
+            }
+            console.log(subPriceBebas, this.props.email)
+            return this.props.applySub(subPriceBebas, this.props.email)
+        }
+        var subPrice = this.refs.nominal.value
+        console.log(subPrice)
         this.props.applySub(subPrice, this.props.email)
     }
 
+    handleChange = () => {
+        var check = this.state.lain
+        this.setState({ lain: !check })
+    } 
+
     render() { 
+        console.log(this.props.email)
+        if(this.state.redirectHome){
+            return(
+                <Redirect to='/login' />
+            )
+        }
         if(this.props.subStatus === 1){
             return(
-                <div>
-                    subscriptionStatus: {this.props.subStatus}
-
-                    subscriptionNominal: Rp. {Numeral(this.props.subNominal).format(0,0)}
-                </div>
+                <div className='container'>
+                <form style={{width: '100%'}}>
+                    <div className='form-control'>
+                        Nominal langganan anda adalah : Rp. {Numeral(this.props.subNominal).format('0,0')}
+                    </div>
+                </form>
+            </div>
             )
         }
         return ( 
-            <>
-            <select className='form-control' name="select" ref={(subPrice) => this.subPrice=subPrice} >
-                <option value={100000}>Rp.{Numeral(100000).format('0,0')}</option>
-                <option value={250000}>Rp.{Numeral(250000).format('0,0')}</option>
-                <option value={500000}>Rp.{Numeral(500000).format('0,0')}</option>
-                <option value={750000}>Rp.{Numeral(750000).format('0,0')}</option>
-                <option value={1000000}>Rp.{Numeral(1000000).format('0,0')}</option>
-            </select>
-            <button onClick={this.getSubPrice}>
-                Click
-            </button>
-            </>
+            <div className='container'>
+                <form style={{width: '100%'}}>
+                    <div className='form-group'>
+                    <label for="exampleInputEmail1">Silahkan pilih jumlah nominal langganan</label>
+                        <select className='form-control' name="select" ref='nominal' hidden={this.state.lain}>
+                            <option value={100000}>Rp.{Numeral(100000).format('0,0')}</option>
+                            <option value={250000}>Rp.{Numeral(250000).format('0,0')}</option>
+                            <option value={500000}>Rp.{Numeral(500000).format('0,0')}</option>
+                            <option value={750000}>Rp.{Numeral(750000).format('0,0')}</option>
+                            <option value={1000000}>Rp.{Numeral(1000000).format('0,0')}</option>
+                        </select>
+                        <input type='number' hidden={!this.state.lain} defaultValue={10000} className='form-control' ref='nominalBebas'/>
+                            <Switch 
+                                onChange={this.handleChange}
+                                inputProps={{ 'aria-label' : 'secondary checkbox' }}
+                            />
+                    </div>
+                    <div className='form-group'>
+                        <div className='d-flex justify-content-center'>
+                            <input type='button' onClick={this.getSubPrice} className='btn btn-primary' value='Berlangganan Sekarang' />
+                                
+                        </div>
+                    </div>
+                </form>
+            </div>
          );
     }
 }
