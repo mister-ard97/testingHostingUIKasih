4d733d15eb1963a993f1198a1abbb19e34395428
Enverd <yuvensenverd@yahoo.com>
2019-10-18 15:28:05 +0700

    Update Subscription.jsx

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -4,11 +4,14 @@ import { connect } from 'react-redux'
 import { applySub, getSub } from '../redux/actions' 
 import { Redirect } from 'react-router-dom'
 import { Switch } from '@material-ui/core'
+import { InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
 
 class Subscription extends Component {
     state = { 
         redirectHome: false,
-        lain : false
+        lain : false,
+        nominal : 0,
+        nominalDisplay : '0'
     }
 
     componentDidMount(){
@@ -18,17 +21,30 @@ class Subscription extends Component {
         this.props.getSub(this.props.email)
     }
 
+    formatDisplay (num) {
+        let number = parseInt(num.split(',').join('')) 
+
+        if (num.split(',').join('') === '' ) {
+            this.setState({nominalDisplay: '0', nominal: 0})
+        } else {
+            this.setState({
+                nominalDisplay: number.toLocaleString(),
+                nominal: number
+            })
+        }
+    }
+
     getSubPrice = () => {
         if(!this.props.email){
             return null
         }
         if(this.state.lain){
-            var subPriceBebas = this.refs.nominalBebas.value
-            if(subPriceBebas < 10000){
+            // var subPriceBebas = this.refs.nominalBebas.value
+            if(this.state.nominal < 10000){
                 return window.alert('Harus diatas Rp. 10.000')
             }
-            console.log(subPriceBebas, this.props.email)
-            return this.props.applySub(subPriceBebas, this.props.email)
+            
+            return this.props.applySub(this.state.nominal, this.props.email)
         }
         var subPrice = this.refs.nominal.value
         console.log(subPrice)
@@ -39,6 +55,11 @@ class Subscription extends Component {
         var check = this.state.lain
         this.setState({ lain: !check })
     } 
+
+    
+    allowPositivesOnly(event) {
+        return (event.keyCode? (parseInt(event.keyCode) === 69 ? false : event.keyCode >= 48 && event.keyCode <= 57) : (event.charCode >= 48 && event.charCode <= 57))? true : event.preventDefault();
+    }
 
     render() { 
         console.log(this.props.email)
@@ -70,11 +91,18 @@ class Subscription extends Component {
                             <option value={750000}>Rp.{Numeral(750000).format('0,0')}</option>
                             <option value={1000000}>Rp.{Numeral(1000000).format('0,0')}</option>
                         </select>
-                        <input type='number' hidden={!this.state.lain} defaultValue={10000} className='form-control' ref='nominalBebas'/>
+                        <InputGroup>
+        <InputGroupAddon addonType="prepend">
+          <Button className="bg-white text-dark border-right-0"  hidden={!this.state.lain} disabled style={{borderColor : '#CED4DA' , border : '1px solid #CED4DA', opacity: 1}}>Rp. </Button>
+        </InputGroupAddon>
+        <Input style={{border : '1px 1px 1px 0 solid #CED4DA'}} hidden={!this.state.lain} ref='nominalBebas' onChange={(text)=>this.formatDisplay(text.target.value)} onKeyPress={this.allowPositivesOnly} value={this.state.nominalDisplay}/>
+      </InputGroup>
+                        {/* <input type='text' hidden={!this.state.lain} defaultValue={`Rp. ${this.state.nominalDisplay}`} className='form-control' ref='nominalBebas' onChange={(text)=>this.formatDisplay(text.target.value)} onKeyPress={this.allowPositivesOnly}/> */}
                             <Switch 
                                 onChange={this.handleChange}
                                 inputProps={{ 'aria-label' : 'secondary checkbox' }}
                             />
+                        <span className="text-gray">Klik Untuk Pilih Nominal </span>
                     </div>
                     <div className='form-group'>
                         <div className='d-flex justify-content-center'>
@@ -82,6 +110,7 @@ class Subscription extends Component {
                                 
                         </div>
                     </div>
+      
                 </form>
             </div>
          );
