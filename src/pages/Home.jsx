60187d54fdecbb797300dfc2a99b59eb76553279
Enverd <yuvensenverd@yahoo.com>
2019-11-06 15:46:54 +0700

    ABOUT US MOBILE
    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -653,12 +653,17 @@ class Home extends Component {
                 {/* About Us */}
                 <div className='about-us container-fluid my-4 p-0'>
                     <div className='row m-0 px-5'>
-                        <div className='offset-1 col-10 my-3'>
+                        <div className=' col-12 offset-md-1 col-md-10 my-3'>
                             <div className='card'>
                                 <div className='row'>
-                                    <div className='col-8 ml-5 my-5'>
-                                        <h2 className='mb-3'>ABOUT <br/> US</h2>
-                                        <p className='p-0 m-0' style={{fontSize: '25px'}}>Lorem ipsum dolor 
+                                    {/* <div className="col-sm-1 d-none d-sm-block">
+
+                                    </div> */}
+                                    <div className='col-sm-8  col-12 p-l-65 my-5'>
+                                        
+                                        <h2 className='mb-3 d-sm-block d-none taglinetext'>ABOUT <br/> US</h2>
+                                        <h2 className='mb-3 d-sm-none d-block taglinetext'>ABOUT US </h2>
+                                        <p className='pr-5 m-0 p-sm-0 m-sm-0 descriptiontext' >Lorem ipsum dolor 
                                             sit amet consectetur adipisicing elit. 
                                             Eaque id corporis et sint autem 
                                             Fin tempora cum voluptatibus reprehenderit, 
@@ -666,14 +671,17 @@ class Home extends Component {
                                             eum harum. Cumque officia perspiciatis reprehenderit.
                                         </p>
                                     </div>
-                                    <div className='col-3 d-flex flex-column justify-content-center px-4'>
-                                        <img src={LogoGray} alt='Logo-AboutUs' className='img-fluid' />
+                                    <div className='col-sm-3 d-none d-lg-flex flex-column justify-content-center px-4 py-5 align-items-center'>
+                                        <img src={LogoGray} alt='Logo-AboutUs' className='logo-about-us' />
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>     
+                <div className="d-flex flex-row justify-content-center">
+                    <a href='/' className="learnmorebutton">PELAJARI LEBIH LANJUT</a>
+                </div>
 
                 
 
