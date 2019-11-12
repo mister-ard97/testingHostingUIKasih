897e858867413f9c1fa34294b4b36c263fe23869
hisbu <lab.hisbu@gmail.com>
2019-11-12 12:22:50 +0700

    a

--- src/helpers/Url_API.js
+++ src/helpers/Url_API.js
@@ -4,7 +4,7 @@
 
 
 // export const URL_API = 'http://localhost:2019';
-export const URL_API = 'ec2-52-221-189-87.ap-southeast-1.compute.amazonaws.com';
+export const URL_API = 'http://ec2-52-221-189-87.ap-southeast-1.compute.amazonaws.com:2019';
 
 
 
